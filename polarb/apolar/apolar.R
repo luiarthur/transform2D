7ef88394ccb26035bf4a8d98d2cd82d6e844447d
luiarthur <luiarthur@yahoo.com>
2015-04-16 11:08:21 -0600

    yes

--- polarb/apolar/apolar.R
+++ polarb/apolar/apolar.R
@@ -5,7 +5,44 @@
 ## Source: http://chrisladroue.com/2012/02/polar-histogram-pretty-and-useful/
 ## Modified from original by Christos Hatzis 3.22.2012 (CH)
 ## =============================================================================
-arfPolar <-
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  #http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+apolar <-
     function(
         df,
         binSize=1,
@@ -58,8 +95,7 @@ arfPolar <-
             xmax <- xmin + binSize
             ymin <- affine(1)
             ymax <- affine(1 - value)
-        }
-                   )      
+        })      
 #        # build the guides
 #        guidesDF<-data.frame(
 #            xmin=rep(df$xmin,length(guides)),
--- polarb/apolar/testApolar.R
+++ polarb/apolar/testApolar.R
@@ -1,11 +1,35 @@
-
-source("arfPolar.R")
+source("apolar.R")
 dfx <- data.frame(family=rep(c("I", "II", "III", "IV")[sort(sample(1:4, 10, TRUE))], 5), 
                   item=rep(LETTERS[1:10], 5), 
                   score=rep(letters[1:5], each=10), 
                   value=rep((1:5), each=10) + rnorm(50, 0, .5))
 
-p <- arfPolar(dfx, familyLabel=TRUE, direction="inwards", 
+p <- apolar(dfx, familyLabel=TRUE, direction="inwards", 
               binSize=0.3, spaceBar=0, spaceItem=0.2, spaceFamily=1.0, innerRadius=0.1, nguides=5)
 print(p)
+
+ggplot(dfx)+geom_rect()
+
+gen.df <- function(n=100,b0=c(1,11,20),b1=2) {
+  k <- length(b0)
+  x <- rnorm(k*n,3)
+  y <- x*b1 + diag(k)%x%rep(1,n)%*%b0 + rnorm(n)
+  dfx <- data.frame(x,y,z=rep(1:k,each=n))
+  dfx
+}
+
+dfx <- gen.df(b0=seq(1,100,len=10))
+
+ggplot(dfx,aes(x,y))+geom_point()
+ggplot(dfx,aes(x,y))+geom_point()+coord_polar()
+ggplot(dfx,aes(y,x))+geom_point()
+ggplot(dfx,aes(y,x))+geom_point()+coord_polar()
+
+p <- ggplot(dfx,aes(y,x))+geom_point()+coord_polar(start=0)
+p
+p+xlim(0,2*pi)
+#p <- ggplot(dfx,aes(x))+geom_density()+coord_polar();p # Works
+#p <- ggplot(dfx)+geom_rect(aes(xmin=-3,xmax=3,ymin=-3,ymax=3,fill=z))+coord_polar();p #Stupid...
+#multiplot(p,p)
+
 
